aed8a4f35ab653a38cda0bbf4be005605c5f6367
MarcoCostantini26 <marco.costantini26@gmail.com>
2024-01-11 11:52:02 +0100

    login done

--- src/template/loginForm.php
+++ src/template/loginForm.php
@@ -6,24 +6,45 @@ $err_mess=null;
 if(isset($_SESSION["ID"])) {
     $err_mess="L'utente è già loggato";
 } elseif(isset($_POST["email"])) {
-    $db = new mysqli("127.0.0.1", "root", "", "sounddrift", 3307);
-    $stmt = $db->prepare("SELECT * FROM utente WHERE email=?");
-    $stmt->bind_param("s", $email);
-    $email = $_POST["email"];
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    $db->close();
-    if(is_null($row)) {
-        $show_form = true;
-        $err_mess="L'utente non esiste";
-    } elseif(!password_verify($_POST["password"], $row["Password"])) {
-        $show_form = true;
-        $err_mess= "Password errata";
-    } else {
-        $_SESSION["ID"] = $row["ID"];
-        $_SESSION["username"] = $row["Username"];
-        header("Location: home.php");
+    $db = new mysqli("127.0.0.1", "root", "", "EduBay", 3307);
+
+    if(isset($_["isAdministrator"])){
+
+        $stmt = $db->prepare("SELECT * FROM amministratore WHERE email=?");
+        $stmt->bind_param("s", $email);
+        $email = $_POST["email"];
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $db->close();
+        if(is_null($row)) {
+            $show_form = true;
+            $err_mess="L'utente non esiste";
+        } elseif($_POST["password"] !== $row["Password"]) {
+            $show_form = true;
+            $err_mess= "Password errata";
+        } else {
+            $_SESSION["ID"] = $row["ID"];
+            header("Location: index.php");
+        }
+    }else{
+        $stmt = $db->prepare("SELECT * FROM utente WHERE email=?");
+        $stmt->bind_param("s", $email);
+        $email = $_POST["email"];
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $db->close();
+        if(is_null($row)) {
+            $show_form = true;
+            $err_mess="L'utente non esiste";
+        } elseif($_POST["password"] !== $row["Password"]) {
+            $show_form = true;
+            $err_mess= "Password errata";
+        } else {
+            $_SESSION["ID"] = $row["ID"];
+            header("Location: index.php");
+        }
     }
 } else {
     $show_form = true;
@@ -33,7 +54,8 @@ if($show_form) {
 ?>
 <form method="POST" action="login.php">
     <input type="email" name="email" placeholder="La tua e-mail"/>
-    <input type="password" name="password" placeholder="La tua password"/>
+    <input type="password" name="password" placeholder="La tua password"/></br>
+    <input type="checkbox" name="isAdministrator"> Sono un amministratore</br>
     <input type="submit" value="Login"/>
 </form>
 <?php
